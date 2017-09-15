d1747e06f1f59a890c34397012e6ad3936010d82
laurkim <laurakeem@gmail.com>
2017-09-15 17:18:57 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
   # code here
+  esperanto_alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |phrase|
+    phrase.split("").collect do |char|
+      esperanto_alpha.index(char)
+    end
+  end
 end
